43f4c493bf4c4184327fe4a10b556575968cacb1
ming0614 <3075624299@qq.com>
2021-04-14 16:51:23 +0800

    notebook

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+import { Button } from 'antd-mobile';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Button type="primary">测试</Button>
+    </div>
+  );
+}
+
+export default App;
